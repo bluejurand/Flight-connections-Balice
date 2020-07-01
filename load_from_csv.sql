0bd85106431efb61d833ea2cfbcee9799179a0df
Michal <m.jur11@gmail.com>
2020-07-01 12:23:19 +0200

    Added loading all csv files

--- load_from_csv.sql
+++ load_from_csv.sql
@@ -2,3 +2,51 @@ COPY air_traffic_data_2019_2020 FROM
 'C:/Users/Public/Documents/air_traffic/flightlist_20190101_20190131.csv'
 DELIMITER ',' CSV HEADER;
 --WITH (FORMAT csv);
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20190201_20190228.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20190301_20190331.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20190401_20190430.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20190501_20190531.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20190601_20190630.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20190701_20190731.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20190801_20190831.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20190901_20190930.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20191001_20191031.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20191101_20191130.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20191201_20191231.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20200101_20200131.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20200201_20200229.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20200301_20200331.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20200401_20200430.csv'
+DELIMITER ',' CSV HEADER;
+COPY air_traffic_data_2019_2020 FROM
+'C:/Users/Public/Documents/air_traffic/flightlist_20200501_20200531.csv'
+DELIMITER ',' CSV HEADER;
