f9ea9029b9bcb0f1fad985a446574c6aa65d91ad
Michal <m.jur11@gmail.com>
2020-07-07 22:03:51 +0200

    Change to execute COPY command with file directory as variable

--- load_from_csv.sql
+++ load_from_csv.sql
@@ -6,6 +6,6 @@ BEGIN
 	file_directory := 'C:/Users/Public/Documents/air_traffic/';
 	file_name := 'flightlist_20190101_20190131.csv';
 	FOR file_name IN SELECT * FROM pg_ls_dir('C:/Users/Public/Documents/air_traffic/') LOOP
-		EXECUTE format('COPY air_traffic_data_2019_2020 FROM ''C:/Users/Public/Documents/air_traffic/%s'' WITH CSV HEADER;', file_name);
+		EXECUTE format('COPY air_traffic_data_2019_2020 FROM ''%s%s'' WITH CSV HEADER;', file_directory, file_name);
     END LOOP;
 END; $$;
