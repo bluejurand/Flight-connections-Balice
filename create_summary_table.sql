88fd93901c33e32af50d67218cc7a747c5f56221
Michal <m.jur11@gmail.com>
2020-07-10 14:23:39 +0200

    Removed unnecessary columns in percentage table

--- create_summary_table.sql
+++ create_summary_table.sql
@@ -26,12 +26,28 @@ SELECT "year",
        flights_number,
        lead(flights_number) over (partition by "month" order by "month", "year" desc),
        round((flights_number/((lead(flights_number) over (partition by "month" order by "month", "year" desc))*1.0)-1.0)*100, 2)
+	   as "connections_change_[%]"
 FROM monthly_flights_number;
 
 ALTER TABLE monthly_flights_percentage
 ADD COLUMN id SERIAL PRIMARY KEY;
 
 DELETE FROM monthly_flights_percentage WHERE id % 2 = 0;
+/*
+ALTER TABLE monthly_flights_percentage
+ADD COLUMN "2020/2019_month";
+*/
+UPDATE monthly_flights_percentage
+SET "2020/2019_month" = concat('2020/2019 ', "month");
 
+ALTER TABLE monthly_flights_percentage
+DROP COLUMN "id",
+DROP COLUMN "year",
+DROP COLUMN flights_number,
+DROP COLUMN "lead";
+/*
 SELECT concat('2020/2019 ', "month") as "2020/2019 month", round as "connections change [%]"
+FROM monthly_flights_percentage;
+*/
+SELECT *
 FROM monthly_flights_percentage;
