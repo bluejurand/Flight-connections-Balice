3cf6f120cec95c91e5ea9dc5a3d4b9c02c74cffe
Michal <m.jur11@gmail.com>
2020-07-13 15:46:48 +0200

    Added median off flights and to/from most frequent country

--- create_balice_summary.sql
+++ create_balice_summary.sql
@@ -28,3 +28,60 @@ SELECT * FROM
 ) a
 
 WHERE rank = 1;
+/*
+CREATE FUNCTION _final_median(anyarray) RETURNS float8 AS $$ 
+  WITH q AS
+  (
+     SELECT val
+     FROM unnest($1) val
+     WHERE VAL IS NOT NULL
+     ORDER BY 1
+  ),
+  cnt AS
+  (
+    SELECT COUNT(*) as c FROM q
+  )
+  SELECT AVG(val)::float8
+  FROM 
+  (
+    SELECT val FROM q
+    LIMIT  2 - MOD((SELECT c FROM cnt), 2)
+    OFFSET GREATEST(CEIL((SELECT c FROM cnt) / 2.0) - 1,0)  
+  ) q2;
+$$ LANGUAGE sql IMMUTABLE;
+
+CREATE AGGREGATE median(anyelement) (
+  SFUNC=array_append,
+  STYPE=anyarray,
+  FINALFUNC=_final_median,
+  INITCOND='{}'
+);
+*/
+SELECT median(flight_length)
+FROM balice_flights_2019_2020;
+
+SELECT * FROM 
+(
+    SELECT
+        SUBSTRING(destination, 2, 1),
+        count(*) cnt,
+        rank() over (ORDER BY count(*) DESC)
+    FROM balice_flights_2019_2020
+    WHERE destination != 'EPKK'
+    GROUP BY SUBSTRING(destination, 2, 1)
+) a
+
+WHERE rank = 1;
+
+SELECT * FROM 
+(
+    SELECT
+        SUBSTRING(origin, 2, 1),
+        count(*) cnt,
+        rank() over (ORDER BY count(*) DESC)
+    FROM balice_flights_2019_2020
+    WHERE origin != 'EPKK'
+    GROUP BY SUBSTRING(origin, 2, 1)
+) a
+
+WHERE rank = 1;
