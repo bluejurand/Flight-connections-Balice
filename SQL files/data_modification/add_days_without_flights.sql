a9e48b95c68ecb4a71c78242caa62bf5e5797e1e
Michal <m.jur11@gmail.com>
2020-07-31 18:22:49 +0200

    Added comment explaining addition of days to firstseen column.

--- SQL files/data_modification/add_days_without_flights.sql
+++ SQL files/data_modification/add_days_without_flights.sql
@@ -1,3 +1,4 @@
+--Added days without flights for analyzed range to show lack of flights in animation
 INSERT INTO balice_flights_2019_2020 (firstseen)
 VALUES
 	(generate_series(timestamp '2020-04-15 00:00:00', '2020-05-31 00:00:00', '1 day'));
